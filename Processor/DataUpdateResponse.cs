2ec9bb3b361b4649098eec330b21530d4acb75e5
GuMiner <gus.gran@gmail.com>
2018-01-10 21:41:11 -0800

    Significantly cleaning up the PieActivityProcessor in preparation for adding history support.

---
+++ Processor/DataUpdateResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace PieBot
+{
+    /// <summary>
+    /// Defines a response that may contain a data update.
+    /// </summary>
+    public class DataUpdateResponse
+    {
+        public DataUpdateResponse(bool updateData, string response)
+        {
+            this.UpdateData = updateData;
+            this.Response = response;
+        }
+
+        public bool UpdateData { get; }
+
+        public string Response { get; }
+    }
+}
--- Processor/PieActivityProcessor.cs
+++ Processor/PieActivityProcessor.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using BotCommon;
 using BotCommon.Activity;
 using System.Linq;
 using BotCommon.Storage;
@@ -17,119 +16,84 @@ namespace PieBot
         public async Task<ActivityResponse> ProcessActivityAsync(IStore blobStore, ActivityRequest request)
         {
             PieBotData pies = await this.GetOrCreatePiesAsync(blobStore).ConfigureAwait(false);
-            bool dataUpdate = false;
-            
 
+            // Preprocess input text
             string[] input = request.SanitizedText.Split(new[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
             if (input.Length == 0)
             {
                 return new ActivityResponse("I could not understand the action to take on the pies.");
             }
 
-            string responseText = string.Empty;
-            if (input[0].Equals("add", StringComparison.OrdinalIgnoreCase))
-            {
-                responseText = AddPieAction(pies.KnownPies, ref dataUpdate, input);
-            }
-            else if (input[0].Equals("vote", StringComparison.OrdinalIgnoreCase) || input[0].Equals("upvote", StringComparison.OrdinalIgnoreCase))
-            {
-                string pieName = string.Join(" ", input.Skip(1));
-                if (string.IsNullOrWhiteSpace(pieName))
-                {
-                    responseText = "I did not receive a pie to vote on.";
-                }
-                else if (!pies.KnownPies.Any(pie => pie.Name.Equals(pieName, StringComparison.OrdinalIgnoreCase)))
-                {
-                    responseText = $"I could not find '{pieName}' pie in the request list.";
-                }
-                else
-                {
-                    Pie pie = pies.KnownPies.First(p => p.Name.Equals(pieName, StringComparison.OrdinalIgnoreCase));
-                    string userName = request.From;
+            string command = input.First().ToLowerInvariant(); // All the commands are in ASCII, so we can use ToLower instead of ToUpper
+            IEnumerable<string> arguments = input.Skip(1);
 
-                    bool added;
-                    if (pie.Upvotes.Any(vote => vote.Equals(userName, StringComparison.OrdinalIgnoreCase)))
-                    {
-                        added = false;
-                        pie.Upvotes = pie.Upvotes.Where(vote => !vote.Equals(userName, StringComparison.OrdinalIgnoreCase)).ToList();
-                    }
-                    else
-                    {
-                        added = true;
-                        pie.Upvotes.Add(userName);
-                    }
-
-                    dataUpdate = true;
-                    responseText = $"{(added ? "Added" : "Removed")} an upvote for the '{pieName}' pie, which now has (+{pie.Upvotes.Count}, -{pie.Downvotes.Count}) votes.";
-                }
-            }
-            else if (input[0].Equals("downvote", StringComparison.OrdinalIgnoreCase))
+            // Perform the requested command
+            DataUpdateResponse response;
+            switch (command)
             {
-                string pieName = string.Join(" ", input.Skip(1));
-                if (string.IsNullOrWhiteSpace(pieName))
-                {
-                    responseText = "I did not receive a pie to vote on.";
-                }
-                else if (!pies.KnownPies.Any(pie => pie.Name.Equals(pieName, StringComparison.OrdinalIgnoreCase)))
-                {
-                    responseText = $"I could not find '{pieName}' pie in the request list.";
-                }
-                else
-                {
-                    Pie pie = pies.KnownPies.First(p => p.Name.Equals(pieName, StringComparison.OrdinalIgnoreCase));
-                    string userName = request.From;
-
-                    bool added;
-                    if (pie.Downvotes.Any(vote => vote.Equals(userName, StringComparison.OrdinalIgnoreCase)))
-                    {
-                        added = false;
-                        pie.Downvotes = pie.Downvotes.Where(vote => !vote.Equals(userName, StringComparison.OrdinalIgnoreCase)).ToList();
-                    }
-                    else
-                    {
-                        added = true;
-                        pie.Downvotes.Add(userName);
-                    }
-
-                    dataUpdate = true;
-                    responseText = $"{(added ? "Added" : "Removed")} a downvote for the '{pieName}' pie, which now has (+{pie.Upvotes.Count}, -{pie.Downvotes.Count}) votes.";
-                }
+                case "add":
+                    response = AddPieAction(pies.KnownPies, arguments);
+                    break;
+                case "vote":
+                case "upvote":
+                    response = VotePieAction(pies.KnownPies, request.From, "upvote", (pie) => pie.Upvotes, arguments);
+                    break;
+                case "dvote":
+                case "downvote":
+                    response = VotePieAction(pies.KnownPies, request.From, "downvote", (pie) => pie.Downvotes, arguments);
+                    break;
+                case "clearvote":
+                    response = ClearVoteAction(pies.KnownPies, arguments);
+                    break;
+                case "rank":
+                    response = new DataUpdateResponse(false, RankAction(pies.KnownPies, input));
+                    break;
+                case "list":
+                    response = new DataUpdateResponse(false, "**Pies**:\n\n" + string.Join("\n\n", pies.KnownPies.OrderBy(pie => pie.Name).Select(pie => pie.Name)));
+                    break;
+                case "history":
+                    response = new DataUpdateResponse(false, "TODO");
+                    break;
+                case "help":
+                    response = new DataUpdateResponse(false, HelpAction());
+                    break;
+                default:
+                    response = new DataUpdateResponse(false, "I did not understand the action you want me to take on pies!");
+                    break;
             }
-            else if (input[0].Equals("rank", StringComparison.OrdinalIgnoreCase))
-            {
-                responseText = RankAction(pies.KnownPies, input);
-            }
-            else if (input[0].Equals("list", StringComparison.OrdinalIgnoreCase))
-            {
-                responseText = "**Pies**:\n\n" + string.Join("\n\n", pies.KnownPies.OrderBy(pie => pie.Name).Select(pie => pie.Name));
-            }
-            else if (input[0].Equals("clearvote", StringComparison.OrdinalIgnoreCase))
-            {
-                responseText = ClearVoteAction(pies.KnownPies, ref dataUpdate, input);
-            }
-            else if (input[0].Equals("history", StringComparison.OrdinalIgnoreCase))
-            {
-                responseText = "No history yet! WIP!";
-            }
-            else if (input[0].Equals("help", StringComparison.OrdinalIgnoreCase))
-            {
-                responseText = HelpAction();
-            }
-            else if (input[0].Equals("debug", StringComparison.OrdinalIgnoreCase))
-            {
-                responseText = request.ChannelId + "|" + request.ConversationId + "|" + request.From + "|" + request.FromId + "|" + request.UserId + "|" + request.IsGroup + "|" + request.Recipient;
-            }
-            else
-            {
-                responseText = "I did not understand the action to take on pies!";
-            }
-
-            if (dataUpdate)
+            
+            // Save if necessary and return
+            if (response.UpdateData)
             {
                 await blobStore.CreateOrUpdateAsync(this.GeneralContainer, this.PieBlob, pies).ConfigureAwait(false);
             }
 
-            return new ActivityResponse(string.Join("\r\n\r\n", responseText));
+            return new ActivityResponse(response.Response);
+        }
+
+        private DataUpdateResponse VotePieAction(List<Pie> pies, string userName, string voteTypeName, Func<Pie, List<string>> pieVoteTypeSelector, IEnumerable<string> arguments)
+        {
+            return FindPieWrapper(
+                pies,
+                arguments,
+                voteTypeName,
+                (pie) =>
+                {
+                    bool added;
+                    if (pieVoteTypeSelector(pie).Any(vote => vote.Equals(userName, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        added = false;
+                        pieVoteTypeSelector(pie).RemoveAll(vote => vote.Equals(userName, StringComparison.OrdinalIgnoreCase));
+                    }
+                    else
+                    {
+                        added = true;
+                        pieVoteTypeSelector(pie).Add(userName);
+                    }
+                    
+                    return new DataUpdateResponse(true, $"{(added ? "Added" : "Removed")} a {voteTypeName} for the '{pie.Name}' pie, which now has (+{pie.Upvotes.Count}, -{pie.Downvotes.Count}) votes.");
+                },
+                (pieName) => new DataUpdateResponse(false, $"I could not find '{pieName}' pie to {voteTypeName}"));
         }
 
         private async Task<PieBotData> GetOrCreatePiesAsync(IStore blobStore)
@@ -151,32 +115,24 @@ namespace PieBot
             return pies;
         }
 
-        private static string AddPieAction(List<Pie> pies, ref bool dataUpdate, string[] input)
+        private static DataUpdateResponse AddPieAction(List<Pie> pies, IEnumerable<string> arguments)
         {
-            string responseText;
-            string pieName = string.Join(" ", input.Skip(1));
-            if (string.IsNullOrWhiteSpace(pieName))
-            {
-                responseText = "I did not receive a pie to add to the request list.";
-            }
-            else if (pies.Any(pie => pie.Name.Equals(pieName, StringComparison.OrdinalIgnoreCase)))
-            {
-                responseText = $"I already have a '{pieName}' pie in the request list.";
-            }
-            else
-            {
-                dataUpdate = true;
-                pies.Add(new Pie()
+            return FindPieWrapper(
+                pies,
+                arguments,
+                "add to the pie suggestion list",
+                (pie) => new DataUpdateResponse(false, $"I already have a '{pie.Name}' pie in the request list."),
+                (pieName) =>
                 {
-                    Name = pieName,
-                    Upvotes = new List<string>(),
-                    Downvotes = new List<string>(),
+                    pies.Add(new Pie()
+                    {
+                        Name = pieName,
+                        Upvotes = new List<string>(),
+                        Downvotes = new List<string>(),
+                    });
+
+                    return new DataUpdateResponse(true, $"I've added a '{pieName}' pie to the request list with zero upvotes and downvotes.");
                 });
-
-                responseText = $"I've added a '{pieName}' pie to the request list with zero upvotes and downvotes";
-            }
-
-            return responseText;
         }
 
         private static string RankAction(List<Pie> pies, string[] input)
@@ -202,40 +158,59 @@ namespace PieBot
             return responseText;
         }
 
+        private static DataUpdateResponse ClearVoteAction(List<Pie> pies, IEnumerable<string> arguments)
+        {
+            return FindPieWrapper(
+                pies,
+                arguments,
+                "clear votes for",
+                (pie) =>
+                {
+                    pie.Downvotes.Clear();
+                    pie.Upvotes.Clear();
+                    return new DataUpdateResponse(true, $"All votes were cleared from '{pie.Name}' pie");
+                },
+                (pieName) => new DataUpdateResponse(false, $"I could not find the '{pieName}' pie to clear"));
+        }
+        
         private static string HelpAction()
         {
             return "PiBot is a distributed pie request submission and voting system, as requested by Helen Huang." +
-                                "\n\n **list** --Lists the names of all pies known to *piebot*" +
-                                "\n\n **vote NameOfPie** -- Upvotes a pie request. **upvote** is a valid synonym. Voting twice will clear your upvote." +
-                                "\n\n **downvote NameOfPie** -- Downvotes a pie request. Downvoting twice will clear your downvote. You are allowed to both upvote and downvote a pie." +
-                                "\n\n **rank** -- Lists all pies in ranked order, based on upvotes minus downvotes. Skips pies with no votes." +
-                                "\n\n **rank detailed** -- Lists all pies in ranked order, including who voted for and against each pie. Skips pies with no votes." +
-                                "\n\n **rank detailed all** -- Lists all pies in ranked order, including who voted for and against each pie. Includes pies with no votes.";
+                   "\n\n **list** --Lists the names of all pies known to *piebot*" +
+                   "\n\n **vote NameOfPie** -- Upvotes a pie request. **upvote** is a valid synonym. Voting twice will clear your upvote." +
+                   "\n\n **downvote NameOfPie** -- Downvotes a pie request. Downvoting twice will clear your downvote. You are allowed to both upvote and downvote a pie." +
+                   "\n\n **rank** -- Lists all pies in ranked order, based on upvotes minus downvotes. Skips pies with no votes." +
+                   "\n\n **rank detailed** -- Lists all pies in ranked order, including who voted for and against each pie. Skips pies with no votes." +
+                   "\n\n **rank detailed all** -- Lists all pies in ranked order, including who voted for and against each pie. Includes pies with no votes." +
+                   "\n\n **history** -- Lists all pies created from these suggestions." +
+                   "\n\n **help** -- Displays how to use the PieBot interface. Shows this help text.";
         }
 
-        private static string ClearVoteAction(List<Pie> pies, ref bool dataUpdate, string[] input)
+        /// <summary>
+        /// Given a series of pies and their arguments, either fails to parse the pie name, finds a pie with the pie name, or doesn't find a pie with the pie name.
+        /// </summary>
+        private static DataUpdateResponse FindPieWrapper(
+            List<Pie> pies,
+            IEnumerable<string> arguments,
+            string pieNameEmptyMessageAction,
+            Func<Pie, DataUpdateResponse> existingPieAction,
+            Func<string, DataUpdateResponse> missingPieAction)
         {
-            string responseText;
-            string pieName = string.Join(" ", input.Skip(1));
+            Pie existingPie = null;
+
+            string pieName = string.Join(" ", arguments);
             if (string.IsNullOrWhiteSpace(pieName))
             {
-                responseText = "I did not receive the pie to clear.";
+                return new DataUpdateResponse(false, $"I did not receive a pie to {pieNameEmptyMessageAction}.");
             }
-            else if (!pies.Any(pie => pie.Name.Equals(pieName, StringComparison.OrdinalIgnoreCase)))
+            else if ((existingPie = pies.FirstOrDefault(pie => pie.Name.Equals(pieName, StringComparison.OrdinalIgnoreCase))) != null)
             {
-                responseText = $"I could not find the pie to clear";
+                return existingPieAction(existingPie);
             }
             else
             {
-                dataUpdate = true;
-                Pie pie = pies.First(p => p.Name.Equals(pieName, StringComparison.OrdinalIgnoreCase));
-                pie.Downvotes = new List<string>();
-                pie.Upvotes = new List<string>();
-
-                responseText = $"All votes were cleared from '{pieName}' pie";
+                return missingPieAction(pieName);
             }
-
-            return responseText;
         }
     }
 }
